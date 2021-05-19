c8d0e617c375319c422a5856d4529db88eb61eed
Nikolay Petrov <nlp@phys.uni-sofia.bg>
2021-05-19 09:18:37 +0300

    Add Human class

---
+++ UUN/Humans.Earth/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Humans.Earth
+{
+    internal abstract class Human
+    {
+        bool ExpressOppinion(string theme)
+        {
+            bool result = false;
+            if (theme.Length > 100)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
