a527b06fd2062dbf5da708f5793fd9d06b2fa1b8
SamuilBachev <sbachev@yahoo.com>
2021-05-19 09:20:51 +0300

    Human on mars

---
+++ UUN/Human.Mars/Ambasdor.cs
@@ -0,0 +1,18 @@
+﻿using Common.Tools.Contracts;
+using System;
+
+namespace Human.Mars
+{
+    public class Ambasdor : IAmbassador
+    {
+        public void Speak()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Vote()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
