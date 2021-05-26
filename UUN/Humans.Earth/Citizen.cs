ea190124194f2acc4e33f8510111fe2165d521ed
Nikolay Petrov <nlp@phys.uni-sofia.bg>
2021-05-26 08:43:49 +0300

    add Citizen, Referendum classes and IElections interface

---
+++ UUN/Humans.Earth/Citizen.cs
@@ -0,0 +1,33 @@
+﻿using Humans.Earth.Democracy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Humans.Earth
+{
+    internal class Citizen : Human
+    {
+        internal string Town { get; set; }
+        internal override string Name { get; set; }
+        public Citizen(string name)
+        {
+            Name = name;
+        }
+
+        internal override void Speak()
+        {
+            Console.WriteLine($"Citizen {Name} is speaking...");
+        }
+
+        internal bool ReferendumVote(Referendum referendum)
+        {
+            bool vote = false;
+            if (referendum.Question.Length > 15)
+            {
+                vote = true;
+            }
+
+            return vote;
+        }
+    }
+}
---
+++ UUN/Humans.Earth/Contracts/IElections.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Humans.Earth.Contracts
+{
+    interface IElections
+    {
+        List<Citizen> Electorate { get; set; }
+
+        void OrganizeElections();
+    }
+}
